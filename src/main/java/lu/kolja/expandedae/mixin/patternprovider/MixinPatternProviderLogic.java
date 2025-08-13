d36a650466d39ddc3d910f48080002fca107211a
Kolja <104643407+Thatguyello@users.noreply.github.com>
2025-08-13 16:30:46 +0200

    add nullcheck incase task doesnt exist

--- src/main/java/lu/kolja/expandedae/mixin/patternprovider/MixinPatternProviderLogic.java
+++ src/main/java/lu/kolja/expandedae/mixin/patternprovider/MixinPatternProviderLogic.java
@@ -293,7 +293,10 @@ public abstract class MixinPatternProviderLogic implements IUpgradeableObject, I
         getGrid().getCraftingService().getCpus().stream()
                 .filter(ICraftingCPU::isBusy)
                 .map(cpu -> (CraftingCPUCluster) cpu)
-                .filter(cluster -> ((AccessorExecutingCraftingJob) ((AccessorCraftingCpuLogic) cluster.craftingLogic).getJob()).getTasks().get(details).getValue() <= 1)
+                .filter(cluster -> {
+                    var task = ((AccessorExecutingCraftingJob) ((AccessorCraftingCpuLogic) cluster.craftingLogic).getJob()).getTasks().get(details);
+                    return task != null && task.getValue() <= 1;
+                })
                 .findFirst()
                 .ifPresent(ICraftingCPU::cancelJob);
     }
